f7ff0f1a6c394b3a6db87ba81550118252d1850f
illyse <ischram@kpmg.com.au>
2020-10-29 17:13:57 +1100

    with old population summary code

--- pof/illyse_to_csv.py
+++ pof/illyse_to_csv.py
@@ -10,13 +10,30 @@ from pof.loader.poles_fleet_data_loader import PolesFleetDataLoader
 print("starting")
 pfd = PolesFleetDataLoader()
 print("between")
-g = pfd.get_fleet_data()
+fd = pfd.get_fleet_data()
 print("okay")
+
+attributes = {
+    "pole_material": [],
+    "pole_strength": [],
+    "age": [],
+    "DAGD_perfect_condition": [],
+    "DCZD_condition_loss": [True],
+    "DPWT_perfect_condition": [],
+    "total_csq": [],
+}
+
+remove = None
+summary = fd.get_population_summary(by=attributes, remove=remove, n_bins=10)
+print("omg it worked")
+x = summary.compute()
+print("omg it really worked")
+
 
 # add progress bar
 # tqdm - simple but may not work with dask
 # Illyse script
-# converted intp csv
+# DONE: converted intp csv
 # input and output tkinter - for script
 # docstring
 # check memory error
